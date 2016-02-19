2a6cd9fdc4efdd374668d0fe856e2c5bab0c7fe4
qitianjin <jianbinduan2006@gmail.com>
2016-02-19 14:01:36 +0800

    https://git.oschina.net/qitianjin/Test01.git

---
+++ ScrollPageDown_Demo/MJRefresh/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  ScrollPageDown_Demo
+//
+//  Created by qitianjin on 16/2/19.
+//  Copyright © 2016年 LJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIView
+
+@end
---
+++ ScrollPageDown_Demo/MJRefresh/test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  ScrollPageDown_Demo
+//
+//  Created by qitianjin on 16/2/19.
+//  Copyright © 2016年 LJH. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
